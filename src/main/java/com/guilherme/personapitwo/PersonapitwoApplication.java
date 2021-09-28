eb46bfc39119d572a36373a0cf4972fb780e75f5
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-09-28 18:20:01 -0300

    First Commit

---
+++ src/main/java/com/guilherme/personapitwo/PersonapitwoApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.personapitwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapitwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapitwoApplication.class, args);
+	}
+
+}
